306942ffd09800251ea503b35d17915359febc97
Francisco Meza <fmezas@gmail.com>
2017-12-05 23:26:43 -0800

    do something

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ var opbeat = require('opbeat').start({
   organizationId: 'e4bf3355c21f486ca74bee8e48a27ac6',
   secretToken: 'b0bd4ad679cbb2506ac244d87333d2a64022b193'
 });
+
+const R = require('ramda');
+
+const name = 'foo';
+const type = 'bar';
+const trans = opbeat.startTransaction(name, type);
+
+const ns = R.times((_) => Math.floor(Math.random() * 1000), 1000);
+console.log(R.reduce((a, n) => a + n, 0, ns));
+
+trans.result = 200;
+trans.end();
