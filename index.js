8c9ce1084856b3e19cea167a96da47668eea89e9
Francisco Meza <fmezas@gmail.com>
2017-12-05 23:52:16 -0800

    give enough time for flushing

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var opbeat = require('opbeat').start({
   appId: 'db20cdd824',
   organizationId: 'e4bf3355c21f486ca74bee8e48a27ac6',
-  secretToken: 'b0bd4ad679cbb2506ac244d87333d2a64022b193'
+  secretToken: 'b0bd4ad679cbb2506ac244d87333d2a64022b193',
+  active: true,
+  instrument: true
 });
 
 const R = require('ramda');
@@ -12,6 +14,7 @@ const type = 'bar';
 const trans = opbeat.startTransaction(name, type);
 
 const trace = opbeat.buildTrace();
+console.log(trace);
 if (trace) { trace.start('doing stuff'); }
 
 const ns = R.times((_) => Math.floor(Math.random() * 1000), 1000);
@@ -21,3 +24,6 @@ if (trace) { trace.end(); }
 
 trans.result = 200;
 trans.end();
+
+setTimeout(() => {console.log('done');}, 60000);
+
